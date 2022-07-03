6afe35ec558fcb56b4ddd925d189c0b474f6629a
Kim Sultan <ssx20010531@gmail.com>
2022-07-03 20:09:42 +0700

    update

--- index.js
+++ index.js
@@ -123,8 +123,8 @@ async function getRandomXP(msg, match) {
 
     var XP_free = Math.floor(Math.random() * 15000) + 1
 
-    var random1 = Math.floor(Math.random() * 200) + 1;
-    var random2 = Math.floor(Math.random() * 100) + 1;
+    var random1 = Math.floor(Math.random() * 111) + 1;
+    var random2 = Math.floor(Math.random() * 131) + 1;
     console.log(random1 + ' & ' + random2);
     if (random1 === random2) {
         try {
