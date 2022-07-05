42b1a99408dec4a6ef1ad7fce250c5bffca65570
Kim Sultan <ssx20010531@gmail.com>
2022-07-05 19:58:10 +0700

    update

--- index.js
+++ index.js
@@ -361,7 +361,7 @@ async function displayLevel(msg, match) {
 
 var now = new Date();
 console.log(now);
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 00, 0, 0) - now;
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 00, 0, 0) - now;
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
 }
